adcbd4942c57149af1f1bb755796566b5c0bdb6e
manuel <josemanuelwar@hotmail.es>
2020-05-16 20:40:59 -0500

    terminado las plantillas

--- resources/views/administrador/Registrofracionamientos.blade.php
+++ resources/views/administrador/Registrofracionamientos.blade.php
@@ -1,3 +1,6 @@
 @include('plantillas.header')
 @include('plantillas.menu')
+@include('plantillas.section')
+<h1>hola</h1>
+@include('plantillas.Endsection')
 @include('plantillas.footer')
---
+++ resources/views/plantillas/section.blade.php
@@ -0,0 +1,4 @@
+      <div class="main-panel">
+        <!-- BEGIN : Main Content-->
+        <div class="main-content">
+          <div class="content-wrapper">
